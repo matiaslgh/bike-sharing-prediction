4b989f0765d75ae06f7cdf45c14f7e8c085a81d2
Matias Luraghi <matiaslgh@gmail.com>
2019-03-19 09:34:35 -0300

    Implement forward_pass_train

--- my_answers.py
+++ my_answers.py
@@ -51,12 +51,14 @@ class NeuralNetwork(object):
         #### Implement the forward pass here ####
         ### Forward pass ###
         # TODO: Hidden layer - Replace these values with your calculations.
-        hidden_inputs = None # signals into hidden layer
-        hidden_outputs = None # signals from hidden layer
+        hidden_inputs = np.dot(X, self.weights_input_to_hidden) # signals into hidden layer
+        hidden_outputs = self.activation_function(hidden_inputs) # signals from hidden layer
 
         # TODO: Output layer - Replace these values with your calculations.
-        final_inputs = None # signals into final output layer
-        final_outputs = None # signals from final output layer
+        final_inputs = np.dot(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
+
+        # This is because the final output won't be between 0 and 1
+        final_outputs = final_inputs # signals from final output layer
 
         return final_outputs, hidden_outputs
 
