2ce56067b2fda9b107c236ceb0373f63069dc81f
Matias Luraghi <matiaslgh@gmail.com>
2019-03-19 23:27:15 -0300

    Set hyperparameters

--- my_answers.py
+++ my_answers.py
@@ -127,7 +127,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 3500
+learning_rate = 0.9
+hidden_nodes = 11
 output_nodes = 1
